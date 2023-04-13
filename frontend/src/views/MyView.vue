cd891a6524a18dee4726613657d28bcbc6d92fb1
G-Lake <note125@naver.com>
2023-04-13 22:09:12 +0900

    input, select tag add
    margin setting

--- frontend/src/views/MyView.vue
+++ frontend/src/views/MyView.vue
@@ -3,7 +3,7 @@
     <div class="myheader">
         <h3>헤더</h3>
     </div>
-    <div class="mypage_content">
+    <div class="mypage_munu_content">
     <div class='left'>
         <ul>
           <li v-for='list in linklist'>
@@ -14,8 +14,17 @@
           </li>
         </ul>
     </div>
-    <div class="mymain">
-        
+    <div class="mypage_content">
+    <div class="search_sort">
+      <input type="text" class="mysearch">
+      <input type="submit" value="검색" class="mysearchbtn">
+      <select class="mysort">
+        <option>최신순</option>
+        <option>추천순</option>
+        <option>결제순</option>        
+      </select>
+    </div>
+    <div class="mymain"> 
         <div class="thumbnail">
             <table>
                 <thead>
@@ -36,6 +45,7 @@
             </table>
         </div>
     </div>
+</div>
 </div>
 </div>
   </template>
@@ -76,14 +86,19 @@
         width: 100%;
         height: 100%;
     }
-    .mypage_content{
+    .mypage_munu_content{
         height: 80%;
         display: flex;
         align-items: center;
         justify-content: center;
     }
+    .mypage_content{
+        width: 100%;
+        height: 100%;
+        margin: 5px 10px 10px 5px;
+    }
     .myheader{
-    height: 50px;
+    height: 80px;
     margin: 10px;
     background:rgba(200, 200, 200, 0.6);  
     display: flex;
@@ -97,9 +112,8 @@
       padding:40px 0; 
       overflow: hidden;
       display: flex;
-      margin:10px;
-      display: flex;
       flex-direction: column;
+      margin: 0 0 10px 10px;
     }
     .left ul {padding: 0 30px;}
     .left ul li {
@@ -108,14 +122,37 @@
         list-style:none;
         font-weight:bold;
     } 
+    .search_sort{
+      width: 100%;
+      height: 70px;
+      background:rgba(235, 235, 235, 0.6); 
+      overflow: hidden;
+      display: flex;
+      margin: 5px 10px 10px 0;
+    }
+    .mysearch{
+      height: 30px;
+      margin: 20px 5px;
+      margin-left: 5%;
+    }
+    .mysearchbtn{
+      height: 30px;
+      margin-top: 20px;
+      margin-bottom: 20px;
+    }
+    .mysort{
+      height: 30px;
+      margin: 20px 5px;
+      margin-left: 10px;
+    }
     .mymain{
       width: 100%;
-      height: 620px;
+      height: 540px;
       background:rgba(235, 235, 235, 0.6); 
       padding:40px 0; 
       overflow: hidden;
       display: flex;
-      margin:10px;
+      margin: 5px 10px 10px 0;
     }
     .thumbnail{
         margin-left: 5%;

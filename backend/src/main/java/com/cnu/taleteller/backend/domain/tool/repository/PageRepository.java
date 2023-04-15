9ae21ae8e75f8b94369579797dd38b8143185ac1
Jungwon  Kim <kimwonny8@gmail.com>
2023-04-15 12:29:11 +0900

    revert
    This reverts commit af3d45e8860c7f10ae8673bbb3ccbc6cb236adcc.

--- backend/src/main/java/com/cnu/taleteller/backend/domain/tool/repository/PageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cnu.taleteller.backend.domain.tool.repository;
-
-import com.cnu.taleteller.backend.domain.tool.domain.Page;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PageRepository extends JpaRepository<Page, Long> {
-}
--- backend/src/test/java/com/cnu/taleteller/backend/controller/ToolControllerTest.java
+++
@@ -1,4 +0,0 @@
-package com.cnu.taleteller.backend.controller;
-
-public class ToolControllerTest {
-}

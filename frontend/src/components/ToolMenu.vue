4b8100551fb3e940df0a4e960cc2a51f1b616b7f
Jungwon  Kim <kimwonny8@gmail.com>
2023-04-26 00:33:42 +0900

    Revert "Modify ToolView, ToolHeader"
    This reverts commit bd3fa8b0838c7c1b38bbc324f48aba0309094c9e.

--- frontend/src/components/ToolMenu.vue
+++ frontend/src/components/ToolMenu.vue
@@ -1,24 +1,48 @@
 <template>
     <div class="menu">
       <div class="menu-list">
-        <button @click="setSelectedMenu('background')">배경</button>
-        <button @click="setSelectedMenu('character')">캐릭터</button>
-        {{ selectedMenu }}
+        <button class="menu-btn" :class="{ active: selectedMenu === 'background' }" @click="setSelectedMenu('background')">배경</button>
+        <button class="menu-btn" :class="{ active: selectedMenu === 'character' }" @click="setSelectedMenu('character')">캐릭터</button>
+        <button class="menu-btn" :class="{ active: selectedMenu === 'scenario' }" @click="setSelectedMenu('scenario')">시나리오</button>
       </div>
-      <div class="image-list">
-        <div class="image-list-char" v-show="selectedMenu == 'character'">
-          <img :src="item.src" :draggable="item.draggable" :id="item.id" :data-id="item.dataId" :style="{ height : item.height}" v-for="item, index in charList">
+      <div class="menu-form">
+        <div class="image-list">
+          <div class="image-list-char" v-show="selectedMenu == 'character'">
+            <img :src="item.src" :draggable="item.draggable" :id="item.id" :data-id="item.dataId" :style="{ height : item.height}" v-for="item, index in charList">
+          </div>
+          <div class="image-list-back" v-show="selectedMenu == 'background'">
+            <img :src="item.src" :draggable="item.draggable" :id="item.id" :data-id="item.dataId" :style="{ height : item.height}" v-for="item, index in backList">
+          </div>
         </div>
-        <div class="image-list-back" v-show="selectedMenu == 'background'">
-          <img :src="item.src" :draggable="item.draggable" :id="item.id" :data-id="item.dataId" :style="{ height : item.height}" v-for="item, index in backList">
-        </div>
-      </div>
-      <div class="uploadImage">
-        <div v-if="selectedMenu == 'background'">
-          <input type="file" @change="setImage('background')" accept="image/*" id="image">
-        </div>
-        <div v-else-if="selectedMenu == 'character'">
-          <input type="file" @change="setImage('character')" accept="image/*" id="image">
+        <div class="uploadImage">
+          <div v-if="selectedMenu == 'background'">
+            <input type="file" @change="setImage('background')" accept="image/*" id="image">
+          </div>
+          <div v-else-if="selectedMenu == 'character'">
+            <input type="file" @change="setImage('character')" accept="image/*" id="image">
+          </div>
+          <!-- 시나리오 -->
+          <div v-else-if="selectedMenu == 'scenario'">
+           <!-- gpt 시나리오 없을 때 -->
+           <div v-if="gpt==true && (!finalScenario || finalScenario.length === 0)">
+              <div class="spinner-border" role="status"></div>
+              <p>열심히 작성중입니다.<br>조금만 기다려주세요!ㅠㅠ</p>
+            </div>
+            <!-- 내가 적은 시나리오 없을 때 -->
+           <div v-else-if="gpt==false && (!finalScenario || finalScenario.length === 0)">
+              <p>입력된 시나리오가 없습니다.<br>시나리오를 입력해주세요.</p>
+              <button>추가</button>
+            </div>
+             <!-- 시나리오 있을 때 -->
+             <div v-else>
+              <p v-for="(story,index) in finalScenario" :key="index">
+                {{ setScenarioLabel(index) }} <br>
+                {{ story }}
+              </p>
+              <button @click="editScenario()">수정</button>
+            </div>
+          </div>
+           
         </div>
       </div>
     </div>
@@ -53,7 +77,7 @@
             dataId : 'i3',
             height : '100px',
           }],
-        selectedMenu: 'character',
+        selectedMenu: 'scenario',
         nextId : 1,
         uploadId : 0,
         isUpload : false,
@@ -62,17 +86,39 @@
     },
     props : {
       currentPageList : Object,
+      finalScenario : Array,
+      gpt: Boolean
     },
     mounted() {
       const toolSelectedPageDrag = document.querySelector('.page-form .selected-page .drag-image');
       this.existingImageEventDragStart();
       this.imageEventDragOver(toolSelectedPageDrag);
       this.imageEventDrop(toolSelectedPageDrag);
+      // console.log(this.gpt);
     },
     updated() {
       this.addImageEventDragstart();
     },
     methods: {
+      // 시나리오 label 나누는 함수
+      setScenarioLabel(index) {
+        switch(index) {
+          case 0:
+            return '[도입]';
+          case 1:
+            return '[전개]';
+          case 2:
+            return '[위기]';
+          case 3:
+            return '[결말]';
+          default:
+            return '';
+        }
+      },
+      // 시나리오 수정
+      editScenario(){
+
+      },
       async setImage(menu) {
         try {
           let frm = new FormData();
@@ -104,12 +150,13 @@
             });
           }
           console.log("전송 성공");
+          document.getElementById("image").value="";
         } catch (e) {
           console.log(e);
         }
       },
       setSelectedMenu(menu) {
-        this.selectedMenu = menu;
+        this.selectedMenu = menu;        
       },
       //기존에 있는 이미지에 dragStart 이벤트를 줌
       existingImageEventDragStart() {
@@ -211,5 +258,23 @@
     align-items: center;
     justify-content: center;
   }
+
+  .menu-form {
+    margin-top: 10px;
+    overflow-y: scroll;
+    height: 90%;
+}
+  .menu-form::-webkit-scrollbar {
+  display: none;
+}
+
+.menu-btn {
+  border: 1px solid gray;
+  background-color: white;
+}
+.menu-btn.active {
+  background-color: gray;
+  color: white;
+}
   </style>
   

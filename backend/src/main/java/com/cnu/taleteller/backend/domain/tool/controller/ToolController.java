55913bf6668da2e580cb0b90712a8cb66778ef5c
Jungwon  Kim <kimwonny8@gmail.com>
2023-05-12 15:45:55 +0900

    test spring data mongodb

--- backend/src/main/java/com/cnu/taleteller/backend/domain/tool/controller/ToolController.java
+++ backend/src/main/java/com/cnu/taleteller/backend/domain/tool/controller/ToolController.java
@@ -1,7 +1,7 @@
 package com.cnu.taleteller.backend.domain.tool.controller;
 
 import com.cnu.taleteller.backend.domain.tool.service.FileService;
-import com.cnu.taleteller.backend.domain.tool.service.ScenarioService;
+import com.cnu.taleteller.backend.domain.tool.service.ToolService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,9 +14,17 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 public class ToolController {
 
-    private final ScenarioService scenarioService;
+//    private final ScenarioService scenarioService;
+
     private final FileService fileService;
 
+    private final ToolService toolService;
+
+    @GetMapping(value = "/save")
+    public void test(@RequestParam String name, @RequestParam int age) {
+        toolService.saveTool(name, age);
+        System.out.println("ok");
+    }
 
     @PostMapping(value = "/image", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<String> saveImage(@RequestParam("image") MultipartFile img, @RequestParam("menu") String menu) {
@@ -38,23 +46,3 @@ public class ToolController {
         }
     }
 }
-//    @PostMapping(value = "/image", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-//    public ResponseEntity<String> saveImage (@RequestParam("image") MultipartFile img, @RequestParam("menu") String menu) {
-//        try {
-//            if (img.isEmpty()) {
-//                throw new IllegalArgumentException("이미지 파일 오류");
-//            }
-//            if (menu == null || menu.isEmpty()) {
-//                throw new IllegalArgumentException("메뉴 오류");
-//            }
-//            String imageFileName = fileService.saveImage(img, menu);
-//            if (imageFileName == null) {
-//                return ResponseEntity.badRequest().body("이미지 파일 업로드 실패");
-//            }
-//            return ResponseEntity.ok(imageFileName);
-//        }
-//        catch (Exception e) {
-//            String msg = "사진 업로드 실패: " + e.getMessage();
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(msg);
-//        }
-//    }
---
+++ backend/src/main/java/com/cnu/taleteller/backend/domain/tool/repository/ToolRepository.java
@@ -0,0 +1,9 @@
+package com.cnu.taleteller.backend.domain.tool.repository;
+
+import com.cnu.taleteller.backend.domain.tool.domain.Tool;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ToolRepository extends MongoRepository<Tool, String> {
+}

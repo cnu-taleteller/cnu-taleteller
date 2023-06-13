c9bf2086929a7b070cf280f281ff3d1c406aa804
Jungwon  Kim <kimwonny8@gmail.com>
2023-06-13 16:42:45 +0900

    Update ToolController.java

--- backend/src/main/java/com/cnu/taleteller/backend/domain/tool/controller/ToolController.java
+++ backend/src/main/java/com/cnu/taleteller/backend/domain/tool/controller/ToolController.java
@@ -17,10 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.Serializable;
-<<<<<<< HEAD
 import java.time.LocalDateTime;
-=======
->>>>>>> parent of c49d01c3 (Add openai controller)
 import java.util.List;
 import java.util.Map;
 
@@ -30,14 +27,9 @@ import java.util.Map;
 public class ToolController {
 
     private final UploadFileService uploadFileService;
-<<<<<<< HEAD
     private final UploadVoiceService uploadVoiceService;
     private final S3Service s3Service;
     private final TTSService ttsService;
-=======
-    private final S3Service s3Service;
-
->>>>>>> parent of c49d01c3 (Add openai controller)
     private final ToolService toolService;
 
     @GetMapping("/s3/image")

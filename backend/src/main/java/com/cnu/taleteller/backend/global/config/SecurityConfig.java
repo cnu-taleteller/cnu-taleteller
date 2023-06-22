3fcd96b00bd0192b52445f9cb2fffb2baeb90f1b
SoonDhi <qmffor004@naver.com>
2023-06-22 16:32:22 +0900

    Security

--- backend/src/main/java/com/cnu/taleteller/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/cnu/taleteller/backend/global/config/SecurityConfig.java
@@ -5,28 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic()
                 .and()
                 .authorizeRequests()
                 .antMatchers("/api/**").permitAll()
                 .and()
-                .csrf().disable()
-        ;
-
-        return http.build();
+                .cors()
+                .and()
+                .csrf().disable();
     }
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {

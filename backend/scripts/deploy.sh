862b79e46eec0441f4e06e60069a3e00930c443b
Jungwon  Kim <86811808+kimwonny8@users.noreply.github.com>
2023-06-26 16:20:01 +0900

    Update deploy.sh

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -9,29 +9,29 @@ APPLICATION_LOG_PATH="/home/ubuntu/app/$PROJECT_NAME/application.log"
 BUILD_JAR=$(ls $JAR_PATH)
 JAR_NAME=$(basename "$BUILD_JAR")
 
-echo "===== 배포 시작 : $(date +%c) =====" >> "$DEPLOY_LOG_PATH"
+echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
 
-echo "> build 파일명: $JAR_NAME" >> "$DEPLOY_LOG_PATH"
-echo "> build 파일 복사" >> "$DEPLOY_LOG_PATH"
-cp "$BUILD_JAR" "$DEPLOY_PATH"
+echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
+echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
+cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 동작중인 어플리케이션 pid 체크" >> "$DEPLOY_LOG_PATH"
-CURRENT_PID=$(pgrep -f "$JAR_NAME")
+echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-if [ -z "$CURRENT_PID" ]; then
-  echo "> 현재 동작중인 어플리케이션이 존재하지 않습니다." >> "$DEPLOY_LOG_PATH"
+if [ -z "$CURRENT_PID" ]
+  echo "> 현재 동작중인 어플리케이션이 존재하지 않습니다." >> $DEPLOY_LOG_PATH
 else
-  echo "> 현재 동작중인 어플리케이션 존재" >> "$DEPLOY_LOG_PATH"
-  echo "> 현재 동작중인 어플리케이션 강제 종료 진행" >> "$DEPLOY_LOG_PATH"
-  echo "> kill 15 $CURRENT_PID" >> "$DEPLOY_LOG_PATH"
-  sudo kill -15 "$CURRENT_PID"
+  echo "> 현재 동작중인 어플리케이션 존재" >> $DEPLOY_LOG_PATH
+  echo "> 현재 동작중인 어플리케이션 강제 종료 진행" >> $DEPLOY_LOG_PATH
+  echo "> kill 15 $CURRENT_PID" >> $DEPLOY_LOG_PATH
+  sudo kill -15 $CURRENT_PID
   sleep 5
 fi
 
 DEPLOY_JAR="$DEPLOY_PATH$JAR_NAME"
-echo "> DEPLOY_JAR 배포" >> "$DEPLOY_LOG_PATH"
-sudo nohup java -jar -Duser.timezone=Asia/Seoul -Dspring.profiles.active=local "$DEPLOY_JAR" --server.port=8080 >> "$APPLICATION_LOG_PATH" 2> "$DEPLOY_ERR_LOG_PATH" &
+echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
+sudo nohup java -jar -Duser.timezone=Asia/Seoul -Dspring.profiles.active=local $DEPLOY_JAR --server.port=8080 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
 
 sleep 3
 
-echo "> 배포 종료 : $(date +%c)" >> "$DEPLOY_LOG_PATH"
+echo "> 배포 종료 : $(date +%c)" >> $DEPLOY_LOG_PATH

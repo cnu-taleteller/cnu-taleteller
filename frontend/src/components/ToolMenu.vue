16f77fa051a25fef62f201f25efcbfd512181278
SoonDhi <qmffor004@naver.com>
2023-10-25 00:56:02 +0900

    Clova Voice Restore
    Clova Voice Restore

--- frontend/src/components/ToolMenu.vue
+++ frontend/src/components/ToolMenu.vue
@@ -93,78 +93,68 @@
             <button class="submit-btn" @click="addTts()">추가</button>
             <p></p><p></p>
           <div class="ttsbtbox">
+
             <div class="ttsbt">
-              <img src="@/assets/people.png" alt="여성1" @click="playSound(1)"><p></p>
-              <input type="radio" name="myRadio" value="noyj" @change="handleTtsChange">
-              <label for="ndain">여성1</label><p></p>
+              <img src="@/assets/people.png" alt="아라" @click="playSound(3)"><p></p>
+              <input type="radio" name="myRadio" value="dara_ang" @change="handleTtsChange">
+              <label for="dara_ang">아라</label>
             </div>
             <div class="ttsbt">
-              <img src="@/assets/people.png" alt="남성1" @click="playSound(2)"><p></p>
-              <input type="radio" name="myRadio" value="nminsang" @change="handleTtsChange">
-              <label for="nsabina">남성1</label>
+              <img src="@/assets/people.png" alt="민영" @click="playSound(4)"><p></p>
+              <input type="radio" name="myRadio" value="nminyoung" @change="handleTtsChange">
+              <label for="nminyoung">민영</label>
             </div>
-<!--            <div class="ttsbt">-->
-<!--              <img src="@/assets/people.png" alt="아라" @click="playSound(3)"><p></p>-->
-<!--              <input type="radio" name="myRadio" value="dara_ang" @change="handleTtsChange">-->
-<!--              <label for="dara_ang">아라</label>-->
-<!--            </div>-->
-<!--            <div class="ttsbt">-->
-<!--              <img src="@/assets/people.png" alt="민영" @click="playSound(4)"><p></p>-->
-<!--              <input type="radio" name="myRadio" value="nminyoung" @change="handleTtsChange">-->
-<!--              <label for="nminyoung">민영</label>-->
-<!--            </div>-->
-<!--            <div class="ttsbt">-->
-<!--              <img src="@/assets/people.png" alt="샤샤" @click="playSound(5)"><p></p>-->
-<!--              <input type="radio" name="myRadio" value="nshasha" @change="handleTtsChange">-->
-<!--              <label for="nshasha">샤샤</label>-->
-<!--            </div>-->
-<!--            <div class="ttsbt">-->
-<!--              <img src="@/assets/people.png" alt="미경" @click="playSound(6)"><p></p>-->
-<!--              <input type="radio" name="myRadio" value="vmikyung" @change="handleTtsChange">-->
-<!--              <label for="vmikyung">미경</label>-->
-<!--            </div>-->
-<!--            <div class="ttsbt">-->
-<!--              <img src="@/assets/people.png" alt="하준" @click="playSound(7)"><p></p>-->
-<!--              <input type="radio" name="myRadio" value="nhajun" @change="handleTtsChange">-->
-<!--              <label for="nhajun">하준</label>-->
-<!--            </div>-->
-<!--            <div class="ttsbt">-->
-<!--              <img src="@/assets/people.png" alt="기태" @click="playSound(8)"><p></p>-->
-<!--              <input type="radio" name="myRadio" value="nkitae" @change="handleTtsChange">-->
-<!--              <label for="nkitae">기태</label>-->
-<!--            </div>-->
-<!--            <div class="ttsbt">-->
-<!--              <img src="@/assets/people.png" alt="기효" @click="playSound(9)"><p></p>-->
-<!--              <input type="radio" name="myRadio" value="nes_c_kihyo" @change="handleTtsChange">-->
-<!--              <label for="nes_c_kihyo">기효</label>-->
-<!--            </div>-->
-<!--            <div class="ttsbt">-->
-<!--              <img src="@/assets/people.png" alt="래원" @click="playSound(10)"><p></p>-->
-<!--              <input type="radio" name="myRadio" value="nraewon" @change="handleTtsChange">-->
-<!--              <label for="nraewon">래원</label>-->
-<!--            </div>-->
-<!--            <div class="ttsbt">-->
-<!--              <img src="@/assets/people.png" alt="악마 마몬" @click="playSound(11)"><p></p>-->
-<!--              <input type="radio" name="myRadio" value="nmammon" @change="handleTtsChange">-->
-<!--              <label for="nmammon">악마 마몬</label>-->
-<!--            </div>-->
-<!--            <div class="ttsbt">-->
-<!--              <img src="@/assets/people.png" alt="영일" @click="playSound(12)"><p></p>-->
-<!--              <input type="radio" name="myRadio" value="nyoungil" @change="handleTtsChange">-->
-<!--              <label for="nyoungil">영일</label>-->
-<!--            </div>-->
-            <audio id="audioElement1" src="@/assets/여성목소리.mp3"></audio>
-            <audio id="audioElement2" src="@/assets/남성목소리.mp3"></audio>
-<!--            <audio id="audioElement3" src="@/assets/아라.mp3"></audio>-->
-<!--            <audio id="audioElement4" src="@/assets/민영.mp3"></audio>-->
-<!--            <audio id="audioElement5" src="@/assets/샤샤.mp3"></audio>-->
-<!--            <audio id="audioElement6" src="@/assets/미경.mp3"></audio>-->
-<!--            <audio id="audioElement7" src="@/assets/하준.mp3"></audio>-->
-<!--            <audio id="audioElement8" src="@/assets/기태.mp3"></audio>-->
-<!--            <audio id="audioElement9" src="@/assets/기효.mp3"></audio>-->
-<!--            <audio id="audioElement10" src="@/assets/래원.mp3"></audio>-->
-<!--            <audio id="audioElement11" src="@/assets/악마 마몬.mp3"></audio>-->
-<!--            <audio id="audioElement12" src="@/assets/영일.mp3"></audio>-->
+            <div class="ttsbt">
+              <img src="@/assets/people.png" alt="샤샤" @click="playSound(5)"><p></p>
+              <input type="radio" name="myRadio" value="nshasha" @change="handleTtsChange">
+              <label for="nshasha">샤샤</label>
+            </div>
+            <div class="ttsbt">
+              <img src="@/assets/people.png" alt="미경" @click="playSound(6)"><p></p>
+              <input type="radio" name="myRadio" value="vmikyung" @change="handleTtsChange">
+              <label for="vmikyung">미경</label>
+            </div>
+            <div class="ttsbt">
+              <img src="@/assets/people.png" alt="하준" @click="playSound(7)"><p></p>
+              <input type="radio" name="myRadio" value="nhajun" @change="handleTtsChange">
+              <label for="nhajun">하준</label>
+            </div>
+            <div class="ttsbt">
+              <img src="@/assets/people.png" alt="기태" @click="playSound(8)"><p></p>
+              <input type="radio" name="myRadio" value="nkitae" @change="handleTtsChange">
+              <label for="nkitae">기태</label>
+            </div>
+            <div class="ttsbt">
+              <img src="@/assets/people.png" alt="기효" @click="playSound(9)"><p></p>
+              <input type="radio" name="myRadio" value="nes_c_kihyo" @change="handleTtsChange">
+              <label for="nes_c_kihyo">기효</label>
+            </div>
+            <div class="ttsbt">
+              <img src="@/assets/people.png" alt="래원" @click="playSound(10)"><p></p>
+              <input type="radio" name="myRadio" value="nraewon" @change="handleTtsChange">
+              <label for="nraewon">래원</label>
+            </div>
+            <div class="ttsbt">
+              <img src="@/assets/people.png" alt="악마 마몬" @click="playSound(11)"><p></p>
+              <input type="radio" name="myRadio" value="nmammon" @change="handleTtsChange">
+              <label for="nmammon">악마 마몬</label>
+            </div>
+            <div class="ttsbt">
+              <img src="@/assets/people.png" alt="영일" @click="playSound(12)"><p></p>
+              <input type="radio" name="myRadio" value="nyoungil" @change="handleTtsChange">
+              <label for="nyoungil">영일</label>
+            </div>
+
+            <audio id="audioElement3" src="@/assets/아라.mp3"></audio>
+            <audio id="audioElement4" src="@/assets/민영.mp3"></audio>
+            <audio id="audioElement5" src="@/assets/샤샤.mp3"></audio>
+            <audio id="audioElement6" src="@/assets/미경.mp3"></audio>
+            <audio id="audioElement7" src="@/assets/하준.mp3"></audio>
+            <audio id="audioElement8" src="@/assets/기태.mp3"></audio>
+            <audio id="audioElement9" src="@/assets/기효.mp3"></audio>
+            <audio id="audioElement10" src="@/assets/래원.mp3"></audio>
+            <audio id="audioElement11" src="@/assets/악마 마몬.mp3"></audio>
+            <audio id="audioElement12" src="@/assets/영일.mp3"></audio>
           </div>
         </div>
         <div class="record" v-else-if="selectedMenu == 'recode'">
